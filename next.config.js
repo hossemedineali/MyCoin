95c8955b7c33d7c3620070a5f46df319e2dbbb8c
hossam ali <hossemedine@outlook.com>
2022-09-18 04:16:05 +0100

    trying to fix deployment errors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
